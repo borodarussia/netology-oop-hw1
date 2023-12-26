66a2e1c0d8193b509423b101c804ca66ddba5314
1 <1>
2023-12-26 09:45:46 +0400

    create fields for methods from task number 4

---
+++ methods_for_task_4.py
@@ -0,0 +1,14 @@
+from class_student import Student
+from class_lecturer import Lecturer
+
+# метод для подсчета средней оценки за домашние задания
+# по всем студентам в рамках конкретного курса
+def determine_average_marks_of_students_for_course(
+        students_list, course_name):
+    pass
+
+# мето для подсчета средней оценки за лекции
+# всех лекторов в рамках курса
+def determine_average_marks_of_lecturers_for_course(
+        lecturers_list, course_name):
+    pass
